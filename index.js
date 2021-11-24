3bb0291f065bc8dbddd72abf5c34604ca723ad25
Kyle Stewart <kylestew@gmail.com>
2021-11-24 15:10:11 -0700

    Need to commit more

--- index.js
+++ index.js
@@ -1,57 +1,53 @@
 import PixelCanvas from "./lib/pixel_canvas";
-import { vec3, color, dot } from "./lib/vec3.js";
-import ray from "./lib/ray.js";
+import { vec3, color } from "./lib/vec3.js";
+import { HittableList } from "./lib/hittable.js";
+import Camera from "./lib/camera.js";
+import Sphere from "./lib/sphere.js";
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 let pixels = PixelCanvas(ctx);
 
-const image_width = canvas.width;
-const image_height = canvas.height;
-const aspect_ratio = image_width / image_height;
+// Image
+const imageWidth = canvas.width;
+const imageHeight = canvas.height;
+const aspectRatio = imageWidth / imageHeight;
 
-const viewport_height = 2.0;
-const viewport_width = aspect_ratio * viewport_height;
-const focal_length = 1.0;
+// World
+let world = HittableList();
+world.add(Sphere(vec3(0, -100.5, -1), 100));
+world.add(Sphere(vec3(0, 0, -1), 0.5));
 
-const origin = vec3();
-const horizontal = vec3(viewport_width, 0, 0);
-const vertical = vec3(0, viewport_height, 0);
-const lower_left_corner = origin
-  .sub(horizontal.divScalar(2))
-  .sub(vertical.divScalar(2))
-  .sub(vec3(0, 0, focal_length));
+// Camera
+const cam = Camera(aspectRatio);
 
-function hitSphere(center, radius, r) {
-  const oc = r.origin.sub(center);
-  const a = dot(r.direction, r.direction);
-  const b = 2.0 * dot(oc, r.direction);
-  const c = dot(oc, oc) - radius * radius;
-  const discriminant = b * b - 4 * a * c;
-  return discriminant > 0;
-}
+function rayColor(ray, world) {
+  let hit = world.hit(ray, 0, Number.MAX_SAFE_INTEGER);
+  if (hit !== false) {
+    // don't have shading yet, map normal to visible color range
+    return hit.normal.add(color(1, 1, 1)).mulScalar(0.5);
+  }
 
-function rayColor(r) {
-  if (hitSphere(vec3(0, 0, -1), 0.5, r)) return color(1, 0, 0);
-
-  const unit_direction = r.direction.unit();
-  const t = 0.5 * (unit_direction.y + 1.0);
+  // default background
+  const unit_direction = ray.direction.unit();
+  let t = 0.5 * (unit_direction.y + 1.0);
   return color(1, 1, 1)
     .mulScalar(1.0 - t)
     .add(color(0.5, 0.7, 1.0).mulScalar(t));
 }
 
+/*
+ * Progressively render scene by randomly scattering rays
+ * TODO: make this smarter (i.e. when are enough rays scattered?)
+ */
 var frameCount = 0;
-const maxFrames = 1000;
+const maxFrames = 400;
 function render() {
-  for (var i = 0; i < 2000; ++i) {
+  for (var i = 0; i < 4000; ++i) {
     const u = Math.random();
     const v = Math.random();
-    const direction = lower_left_corner.add(
-      horizontal.mulScalar(u).add(vertical.mulScalar(v)).sub(origin)
-    );
-    const r = ray.create(origin, direction);
-    const color = rayColor(r);
+    const r = cam.getRay(u, v);
+    const color = rayColor(r, world);
     pixels.write(u, v, color);
   }
   pixels.display();
