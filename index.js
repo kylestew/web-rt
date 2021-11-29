7fffc3aadb6ec022c456fc3aa2b315103b283498
Kyle Stewart <kylestew@gmail.com>
2021-11-29 13:10:51 -0700

    Modernizing my JS a bit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import PixelCanvas from "./lib/pixel_canvas";
-import { vec3, color, randomInUnitSphere } from "./lib/vec3.js";
+import { Vec3, Color } from "./lib/vec3.js";
 import { HittableList } from "./lib/hittable.js";
 import Camera from "./lib/camera.js";
 import Sphere from "./lib/sphere.js";
@@ -13,19 +13,19 @@ let pixels = PixelCanvas(ctx);
 const imageWidth = canvas.width;
 const imageHeight = canvas.height;
 const aspectRatio = imageWidth / imageHeight;
-const samplesPerPixel = 2;
+const samplesPerPixel = 1;
 const maxDepth = 16;
 
 // World
 let world = HittableList();
-world.add(Sphere(vec3(0, -100.5, -1), 100));
-world.add(Sphere(vec3(0, 0, -1), 0.5));
+world.add(Sphere(new Vec3(0, -100.5, -1), 100));
+world.add(Sphere(new Vec3(0, 0, -1), 0.5));
 
 // Camera
 const cam = Camera(aspectRatio);
 
 function rayColor(ray, world, depth) {
-  if (depth <= 0) return color(0, 0, 0);
+  if (depth <= 0) return new Color(0, 0, 0);
 
   let hit = world.hit(ray, 0, Number.MAX_SAFE_INTEGER);
   if (hit !== false) {
@@ -33,17 +33,20 @@ function rayColor(ray, world, depth) {
     // pick a random point S in the unit sphere tangent to hit point P
     // unit sphere is centered at (P + n)
     // (S - P) is the new direction vector of our diffuse reflection
-    let S = hit.p.add(hit.n.add(randomInUnitSphere()));
+    let S = hit.p.add(hit.n.add(Vec3.RandomInUnitSphere()));
     // ray march recusively in new direction
-    return rayColor(Ray(hit.p, S.sub(hit.p)), world, depth - 1).mulScalar(0.5);
+    // (losing some of the light intensity after bounce?)
+    return rayColor(new Ray(hit.p, S.sub(hit.p)), world, depth - 1).mulScalar(
+      0.5
+    );
   }
 
   // default background
   const unit_direction = ray.direction.unit();
   let t = 0.5 * (unit_direction.y + 1.0);
-  return color(1, 1, 1)
+  return new Color(1, 1, 1)
     .mulScalar(1.0 - t)
-    .add(color(0.5, 0.7, 1.0).mulScalar(t));
+    .add(new Color(0.5, 0.7, 1.0).mulScalar(t));
 }
 
 /*
@@ -54,7 +57,7 @@ function render() {
   for (let i = 0; i < imageWidth; ++i) {
     const u = i / imageWidth;
     const v = currentLine / imageHeight;
-    let pixel = color(0, 0, 0);
+    let pixel = new Color(0, 0, 0);
     for (let s = 0; s < samplesPerPixel; ++s) {
       const su = u + Math.random() / imageWidth;
       const sv = v + Math.random() / imageHeight;
